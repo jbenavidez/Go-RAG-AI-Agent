a299dc78e722cec0fad4ef5a7495489c9846c0c7
JBenavidez <jbenavidez@Mac.lan>
2025-12-07 21:24:42 -0500

    cleanup

--- client-service/utils/utils.go
+++ client-service/utils/utils.go
@@ -6,6 +6,8 @@ import (
 )
 
 func SimpleEmbedding(text string) []float64 {
+	// if you have an Open AI Key, you can use openAI Embedding model to make it more robust.
+	// Since this is simple RAG Ai Agent, I will keep it as simple as possible
 	text = strings.ToLower(text)
 	vector := make([]float64, 32)
 
